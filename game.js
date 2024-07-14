c3dd630a784e9651cf5b97c113b428f0d6b02de6
SushiCodelnw <1389Dead@gmail.com>
2024-07-14 17:53:06 +0700

    Add files via upload

--- game.js
+++ game.js
@@ -7,25 +7,43 @@ const word_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 let in_line = true;
 box.addEventListener("click", () => {
   if (in_line == true) {
-    if (time != 2) {
-      document.getElementById("text").innerHTML = word_list[time];
-    } else if (time == 2) {
-        in_line = false;
-        choose_open(1)
-    }
+    document.getElementById("text").innerHTML = word_list[time];
     time += 1;
-  } else if (in_line == false) {
+    if (time == 2) {
+      in_line = false;
+      choose_open(2, "hello", "YO BRO!");
+    }
   }
 });
-function choose_open(number) {
-    if(number == 1){
-        choose1.style.opacity = "1";
-    }else if(number == 2){
-        choose1.style.opacity = "1";
-        choose2.style.opacity = "1";
-    }else if(number == 3){
-        choose1.style.opacity = "1";
-        choose2.style.opacity = "1";
-        choose3.style.opacity = "1";
-    }
+function choose_close() {
+  choose1.style.opacity = "0";
+  choose2.style.opacity = "0";
+  choose3.style.opacity = "0";
+}
+
+function choose_open(number, text1, text2, text3) {
+  switch (number) {
+    case 1:
+      choose1.innerHTML = text1;
+      choose1.style.opacity = "1";
+      break;
+    case 2:
+      choose1.innerHTML = text1;
+      choose2.innerHTML = text2;
+      choose1.style.opacity = "1";
+      choose2.style.opacity = "1";
+      break;
+    case 3:
+      choose1.innerHTML = text1;
+      choose2.innerHTML = text2;
+      choose3.innerHTML = text3;
+      choose1.style.opacity = "1";
+      choose2.style.opacity = "1";
+      choose3.style.opacity = "1";
+      break;
+  }
+  choose1.addEventListener("click", () => {
+    choose_close();
+    in_line = true;
+  });
 }
