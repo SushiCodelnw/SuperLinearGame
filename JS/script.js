84d29a4c315638122d68c71c2b6eec2925a04ca2
SushiCodelnw <1389Dead@gmail.com>
2024-07-27 10:31:38 +0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,12 @@ const box = document.getElementById("box");
 const choose1 = document.getElementById("choose1");
 const choose2 = document.getElementById("choose2");
 const choose3 = document.getElementById("choose3");
+
 let choose1_BooleanOpen = false;
 let choose2_BooleanOpen = false;
 let choose3_BooleanOpen = false;
 let time = 0;
+
 const word_list = [
   "เย้!! ถึงบ้านแล้ว",
   "เอาแหละ ทำอะไรก่อนดี",
@@ -20,13 +22,16 @@ const word_list = [
   "ZZZZZZ",
   "ZZZZZZZ",
 ];
+
 let in_line = true;
+
 box.addEventListener("click", BoxClick);
+
 function BoxClick() {
-  if (in_line == true) {
-    document.getElementById("text").innerHTML = word_list[time];
-    time += 1;
-    if (time == 2) {
+  if (in_line) {
+    document.getElementById("text").innerText = word_list[time];
+    time++;
+    if (time === 2) {
       in_line = false;
       choose_open(
         "นอน",
@@ -48,56 +53,39 @@ function choose_close() {
   choose2_BooleanOpen = false;
   choose3_BooleanOpen = false;
 }
+
 function choose_close_next(outline) {
   choose_close();
-  if (outline == " ") {
+  if (outline === " ") {
     in_line = true;
     BoxClick();
   } else {
-    document.getElementById("text").innerHTML = outline;
+    document.getElementById("text").innerText = outline;
     in_line = true;
   }
 }
 
 function chooseNumber_Open(number, text, outline) {
-  number.innerHTML = text;
+  number.innerText = text;
   number.style.opacity = "1";
-  switch (number) {
-    case choose1:
-      choose1_BooleanOpen = true;
-      number.addEventListener("click", () => {
-        if (choose1_BooleanOpen == true) {
-          choose_close_next(outline);
-        }
-      });
-      break;
-    case choose2:
-      choose2_BooleanOpen = true;
-      number.addEventListener("click", () => {
-        if (choose2_BooleanOpen == true) {
-          choose_close_next(outline);
-        }
-      });
-      break;
-    case choose3:
-      choose3_BooleanOpen = true;
-      number.addEventListener("click", () => {
-        if (choose3_BooleanOpen == true) {
-          choose_close_next(outline);
-        }
-      });
-      break;
+  if (number === choose1) {
+    choose1_BooleanOpen = true;
+  } else if (number === choose2) {
+    choose2_BooleanOpen = true;
+  } else if (number === choose3) {
+    choose3_BooleanOpen = true;
   }
+  number.onclick = () => choose_close_next(outline);
 }
 
 function choose_open(text1, text2, text3, outline1, outline2, outline3) {
-  if (text1 != " ") {
+  if (text1 !== " ") {
     chooseNumber_Open(choose1, text1, outline1);
   }
-  if (text2 != " ") {
+  if (text2 !== " ") {
     chooseNumber_Open(choose2, text2, outline2);
   }
-  if (text3 != " ") {
+  if (text3 !== " ") {
     chooseNumber_Open(choose3, text3, outline3);
   }
 }
